2bf250b25ab909d59cb73f272847c1c522a81dc6
muhammadumarmalik <malikgraphics811@gmail.com>
2023-10-08 22:46:10 +0500

    authentication and cookie session setup with hash

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { IsEmail } from 'class-validator';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
+import { error } from 'console';
 
 const scrypt = promisify(_scrypt);
 
@@ -35,5 +37,27 @@ export class AuthService {
     const user= await this.userService.create(name,address,email,result,phone_number);
     //return the user
     return user;
+  }
+
+
+  async signin(email:string,password:string){
+    const [user]= await this.userService.find(email)
+    if(!user)
+    {
+      throw new NotFoundException('user is not found')
+    }
+
+    const [salt, storedHash]=user.password.split('.');
+
+    
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if(storedHash != hash.toString('hex'))
+    {
+      throw new BadRequestException('bad password');
+    }
+
+    return user;
+     
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,13 @@
 import { AuthService } from './auth.service';
 
-import { Body,Controller, Post,Get,Patch,Delete,Param,Query,NotFoundException,UseInterceptors,ClassSerializerInterceptor} from '@nestjs/common';
+import { Body,Controller, Post,Get,Patch,Delete,Param,Query,NotFoundException,Session} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptor/serlize.interceptor';
 import { UserDto } from './dto/users.dtos';
-
+import { CreateSigninDto } from './dto/create-signin.dto';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
@@ -15,12 +15,36 @@ export class UsersController {
     constructor(private usersService:UsersService,private authService:AuthService){}
     
     @Post('/signup')
-    createUser(@Body() body:CreateUserDto){
-      
-       return this.authService.signup(body.name,body.address,body.email,body.password,body.phone_number)
+    async createUser(@Body() body:CreateUserDto , @Session() session:any ){
+      const user= await this.authService.signup(body.name,body.address,body.email,body.password,body.phone_number)
+        session.userId=user.id;
+        return user;
+   
     }
     // @UseInterceptors(new SerializeInterceptor(UserDto))
     
+
+    @Post('/signin')
+    async signin(@Body() body:CreateSigninDto , @Session() session:any){
+        const user= await this.authService.signin(body.email,body.password)
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get('/whoami')
+    whoAmI(@Session() session:any){
+        return this.usersService.findOne(session.userId)
+    }
+    @Get('/colors/color')
+    setColor(@Param('color') color:string,@Session() session:any){
+        session.color=color;
+    }
+    @Get('/color')
+    getColor(@Session() session:any){
+       return session.color
+    }
+
+
     @Get('/:id')
     
    async findUser(@Param('id') id:string)
